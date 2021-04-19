a8fb597504dde3a3a4d7e7326c0bcd3e558960e2
ZinTelL <82569362+ZinTelL@users.noreply.github.com>
2021-04-19 21:57:13 +0700

    Create Exercise4_Kornrawit_S.py

---
+++ Exercise4_Kornrawit_S.py
@@ -0,0 +1,9 @@
+FounEng = 80.5
+GenBus = 72
+IntroComSys = 78.5
+ComPro = 97.5
+print("----Your Score----")
+print("Foundation English              ", " : ", FounEng,    "/100")
+print("General Business                ", " : ", GenBus,     "/100")
+print("Introdution to Computer Systems ", " : ", IntroComSys,"/100")
+print("Computer Programming            ", " : ", ComPro,     "/100")
