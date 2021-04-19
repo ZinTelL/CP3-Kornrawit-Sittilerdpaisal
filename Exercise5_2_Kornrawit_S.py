aa588929ad5bfdfb70c0a9ee0db8ff0b34e591dc
ZinTelL <82569362+ZinTelL@users.noreply.github.com>
2021-04-19 23:50:04 +0700

    Exercise5_2

---
+++ Exercise5_2_Kornrawit_S.py
@@ -0,0 +1,4 @@
+print("Velocity Calculator Program")
+range = int(input("Please input range in kilometer :"))
+time = int(input("Please input time in hour:"))
+print("Your speed =",range/time,"km/h")
