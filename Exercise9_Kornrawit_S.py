5a65e1ffae5ae0ede4eaa4fe5df1c7bf22a181c1
ZinTelL <82569362+ZinTelL@users.noreply.github.com>
2021-06-07 00:09:18 +0700

    update exercise

---
+++ Exercise9_Kornrawit_S.py
@@ -0,0 +1,6 @@
+usernameInput = input("Username :")
+passwordInput = input("Password :")
+while usernameInput != "admin" or passwordInput !="1234":
+    usernameInput = input("username :")
+    passwordInput = input("password :")
+    print("Login success")
