bc7535b835170d3ac0215f6d0a0634fa3924a30f
ZinTelL <82569362+ZinTelL@users.noreply.github.com>
2021-06-08 22:15:32 +0700

    Funtion plus,minus,multiply,divided

---
+++ Lecture_50_Kornrawit_S.py
@@ -0,0 +1,19 @@
+def plusNumber(x,y):
+   print("x+y:",x+y)
+
+def minusNumber(x,y):
+    print("x-y:", x-y)
+
+def multiplyNumber(x,y):
+    print("x*y:", x*y)
+
+def dividedNumber(x,y):
+    print("x/y=", x/y)
+
+x = int(input("Add first number(x):"))
+y = int(input("Add second number(y):"))
+
+plusNumber(x,y)
+minusNumber(x,y)
+multiplyNumber(x,y)
+dividedNumber(x,y)
